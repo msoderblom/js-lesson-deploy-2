847f4a9762337e3f3dbfb0a273ad65e2a87e08ee
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-29 11:01:45 +0200

    Updates

---
+++ precache-manifest.f884ef9c7b6d3ea27b261e9be72bc847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c326b554dd15766c6292dbd2d50e69",
+    "url": "/js-lesson-deploy-2/index.html"
+  },
+  {
+    "revision": "4e267f09fa246a47a271",
+    "url": "/js-lesson-deploy-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "224c18fde35ae9ef5a22",
+    "url": "/js-lesson-deploy-2/static/js/2.959f1789.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-lesson-deploy-2/static/js/2.959f1789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e267f09fa246a47a271",
+    "url": "/js-lesson-deploy-2/static/js/main.da66291c.chunk.js"
+  },
+  {
+    "revision": "34c0c9107f58c6ce9d3b",
+    "url": "/js-lesson-deploy-2/static/js/runtime-main.3533e071.js"
+  }
+]);
